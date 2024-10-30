d1e93d59ae2f0f0d81995e885f7412f2cb0c58b8
ravensapiens <natsu.graphics@gmail.com>
2024-10-30 20:55:20 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -11,15 +11,6 @@ $(function(){
 	});
 });
 
-$(document).scroll(function() {
-
-	if ( $(document).scrollTop() >= 300 ) {
-		$(".goTop").css("display", "block");
-    } else {
-		$(".goTop").css("display", "none");
-	}
-});
-
 /************* top navigation menu (mobile)  ***************/
 $(function(){
 	$(".nav").on( "click", function() {
@@ -34,3 +25,28 @@ $(function(){
 		}
 	});
 });
+
+$(function () {
+  $(window).scroll(function() {
+  	showElementAnimation();
+  });
+});
+
+function showElementAnimation() {
+	var element = document.getElementsByClassName('scrollShow');
+	if(!element) return;
+
+	var showTiming = window.innerHeight > 300;
+	var scrollY = window.pageYOffset;
+	var windowH = window.innerHeight;
+
+	for(var i=0;i<element.length;i++) { 
+		var elemClientRect = element[i].getBoundingClientRect();
+		var elemY = scrollY + elemClientRect.top;
+		if(scrollY + windowH - showTiming > elemY) { 
+			element[i].classList.add('is-show');
+		} else if(scrollY + windowH < elemY) {
+			element[i].classList.remove('is-show');
+		}
+	}
+}
--- js/works.js
+++ js/works.js
@@ -34,3 +34,28 @@ $(function(){
 		}
 	});
 });
+
+$(function () {
+  $(window).scroll(function() {
+  	showElementAnimation();
+  });
+});
+
+function showElementAnimation() {
+	var element = document.getElementsByClassName('scrollShow');
+	if(!element) return;
+
+	var showTiming = window.innerHeight > 300;
+	var scrollY = window.pageYOffset;
+	var windowH = window.innerHeight;
+
+	for(var i=0;i<element.length;i++) { 
+		var elemClientRect = element[i].getBoundingClientRect();
+		var elemY = scrollY + elemClientRect.top;
+		if(scrollY + windowH - showTiming > elemY) { 
+			element[i].classList.add('is-show');
+		} else if(scrollY + windowH < elemY) {
+			element[i].classList.remove('is-show');
+		}
+	}
+}
