0fa44fd6e1b90bd27a5ea3b8faace7e9fed29c97
ravensapiens <natsu.graphics@gmail.com>
2024-12-04 10:09:28 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 // variables:
-var introAnimation = ["007_3-min.gif"];
+var introAnimation = new Image();
+var loopAnimation = new Image();
+introAnimation.src = "007-min.gif";
+loopAnimation.src = "007-loop-min.gif";
 
 $(document).ready(function() {
 	setIntroAnimation();
+	setTimeout(setLoopAnimation, 6800);
 })
 
 function setIntroAnimation() {
-		$('#sapiens_bond').css("background-image", "url(" + introAnimation[0] + ")");
+	$('#sapiens_bond').css("background-image", "url(" + introAnimation.src + ")");
+
+}
+function setLoopAnimation() {
+	$('#sapiens_bond').css("background-image", "url(" + loopAnimation.src + ")");
 }
