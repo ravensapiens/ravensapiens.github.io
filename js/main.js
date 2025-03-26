5c087293ee98c1c5633acad39095a689e0527c89
ravensapiens <natsu.graphics@gmail.com>
2025-03-26 17:00:04 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,18 +1,11 @@
 // variables:
-var introAnimation = new Image();
 var loopAnimation = new Image();
-introAnimation.src = "007-min.gif";
-loopAnimation.src = "007-loop-min.gif";
+loopAnimation.src = "007.gif";
 
 $(document).ready(function() {
-	setIntroAnimation();
-	setTimeout(setLoopAnimation, 6800);
+	setLoopAnimation();
 })
 
-function setIntroAnimation() {
-	$('#sapiens_bond').css("background-image", "url(" + introAnimation.src + ")");
-
-}
 function setLoopAnimation() {
 	$('#sapiens_bond').css("background-image", "url(" + loopAnimation.src + ")");
 }
